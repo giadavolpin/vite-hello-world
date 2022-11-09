982e0520e0636c2c72db195a4d6ce20c647e38bd
giadavolpin <volpingiada@gmail.com>
2022-11-09 18:10:36 +0100

    fatto

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,31 +1,26 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <span @click="cambiaNome" class="nome">{{ nome }}</span>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<script>
+export default {
+  data() {
+    return {
+      nome: 'Giada',
+    }
+  },
+  methods: {
+    cambiaNome() {
+      this.nome = "Volpin";
+    }
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+</script>
+
+<style lang="css" scoped>
+.nome {
+  color: green;
 }
 </style>
